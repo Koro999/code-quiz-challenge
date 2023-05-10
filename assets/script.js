485be0f950b06a9c6cd406c1358ddd7018c766fa
Koro999 <chris.dang93@hotmail.com>
2023-05-09 20:30:22 -0400

    got questions and possible answers to appear

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,26 @@
-//Query Selector for main cards that need to be navigated through
+//Query Selector for main card containers that need to be navigated through
 var introductionPage = document.querySelector("#introductionPage") //fixed page
 var quizPage = document.querySelector("#quizPage") //needs to be cleared 
 var highscorePage = document.querySelector("#highscorePage") //somethings need to be cleared 
 
 //timer variables 
-var timer = 60; 
+var timer = 6969; 
 var timeStart = document.querySelector('#timer')
 
 //Query Selector values for buttons
 var startQuiz = document.querySelector("#startQuiz")
 
-//quiz questions and answer storage
-var questions = [
+//Score count, 1 per correct answer 
+var score = 0;
+var questionNumber = 0;
+
+//Query Selector for quiz questions, answers, and correct or incorrect 
+var questionSelect = document.querySelector("#question") 
+var answerSelect = document.querySelector("#answers") 
+var rightWrong = document.querySelector("#rightWrong") 
+
+//quiz questions and answer storage in an Array
+const quizQuestions = [
 {
     question: "Which of the following is correct about JavaScript?",
     answers: {
@@ -77,8 +86,14 @@ var questions = [
 //startQuiz button. swap to quiz page, start timer
 startQuiz.addEventListener("click", function(){
     introductionPage.setAttribute("style", "display:none");
+    
+    //setting quizPage to show
     quizPage.setAttribute("style", "display:visible");
+    questionSelect.setAttribute("style", "display:visible");
+    answerSelect.setAttribute("style", "display:visible");
+    rightWrong.setAttribute("style", "display:visible");
 
+    quizGenerate();
 
     //sets timer to one minute to do the quiz 
     timeStart.textContent = timer;
@@ -91,5 +106,69 @@ startQuiz.addEventListener("click", function(){
             quizPage.setAttribute("style", "display:none");
             highscorePage.setAttribute("style", "display:visible"); //lose, still register score 
         }        
-        },1000)
+    },1000)
 })
+
+//show the quiz content on the page and check answers 
+function quizGenerate() {
+    questionSelect.textContent= quizQuestions[questionNumber].question;
+    var buttonNumber = 0;
+
+    for(answer in quizQuestions[questionNumber].answers) {
+        //generate buttonNumber for comparison later
+        buttonNumber++;
+        //console.log(buttonNumber);
+        
+        //create list elements
+        var liElem = document.createElement("li");
+        answerSelect.appendChild(liElem);
+        answerSelect.setAttribute("style", "list-style: none;")
+        answerSelect.setAttribute("class", "answerList")
+        console.log(answerSelect);
+        //create buttons inside the list elements 
+        var button = document.createElement("button")   
+        button.setAttribute("value", buttonNumber)
+        button.innerHTML = `${answer}: ${quizQuestions[questionNumber].answers[answer]}`;     
+        liElem.appendChild(button);
+    }
+
+    //event listener to check if answer clicked is correct 
+    answerSelect.addEventListener("click", function(event) {
+        var element = event.target; 
+
+        const userAnswer = element.getAttribute('value') - 1;
+        const abcd = ["a","b","c","d"];
+
+        if (element.matches("button") === true) {
+            if (abcd[userAnswer] === quizQuestions[questionNumber].correctAnswer){
+                score++;
+                var correct = setInterval(function () {
+                    rightWrong.textContent = "C O R R E C T"
+                    clearInterval(correct);
+                }, 3000);
+                nextQuestion();
+            }
+            else{
+                timer = timer - 10;
+                var wrong = setInterval(function () {
+                    rightWrong.textContent = "I N C O R R E C T"
+                    clearInterval(wrong);
+                }, 3000);
+                nextQuestion();
+            }
+        }
+    })
+
+}
+
+function nextQuestion () {
+    var nextQuestion = setInterval(function(){
+        questionNumber++;
+        for(answer in quizQuestions[questionNumber].answers) {
+            answerList = document.getElementById("answers");
+            answerList.removeChild();
+        }
+        clearInterval(nextQuestion);
+    },3000)
+    quizGenerate ();
+}
