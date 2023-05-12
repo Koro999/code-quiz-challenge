7d2334e58817f3d99f158dcea39a879ca4b13f75
Koro999 <chris.dang93@hotmail.com>
2023-05-12 18:56:42 -0400

    finished commenting JS

--- assets/script.js
+++ assets/script.js
@@ -33,8 +33,6 @@ var questionSelect = document.querySelector("#question") ;
 var answerSelect = document.querySelector("#answers") ;
 var rightWrong = document.querySelector("#rightWrong") ;
 
-
-
 // set an object to store into local storage
 var recordedScore =[[],[]]
 
@@ -102,81 +100,6 @@ const quizQuestions = [
 }
 ]
 
-//show the quiz content on the page and check answers 
-function quizGenerate() {
-    if (questionNumber > quizQuestions.length){ //
-        clearButtons ();
-        return;
-    }
-    timerStop = false;
-    
-    questionSelect.textContent= quizQuestions[questionNumber].question;
-    
-    var buttonNumber = 0;
-    for(answer in quizQuestions[questionNumber].answers) {
-        //generate buttonNumber for comparison later
-        buttonNumber++;
-        //console.log(buttonNumber);
-        
-        //create list elements
-        var liElem = document.createElement("li");
-        answerSelect.appendChild(liElem);
-        answerSelect.setAttribute("style", "list-style: none;");
-        answerSelect.setAttribute("class", "answerList");
-        //console.log(answerSelect);
-        //create buttons inside the list elements 
-        var button = document.createElement("button");
-        button.setAttribute("value", buttonNumber);
-        button.setAttribute("class", "answerButton");
-        button.setAttribute("style", "width: 80%; list-style:none; padding-left:0; text-align: left; position:relative; left:5%");
-        button.innerHTML = `${answer}: ${quizQuestions[questionNumber].answers[answer]}`;     
-        liElem.appendChild(button);
-    }
-    buttonNumber = 0;
-
-    
-    //event listener to check if answer clicked is correct 
-    answerSelect.addEventListener("click", function(event) {
-        var element = event.target; 
-        
-        buttons = document.querySelectorAll('button');
-
-        buttons.forEach((answerButton) => {
-            if( answerButton.getAttribute("class") === "answerButton"){
-                answerButton.setAttribute('disabled', 'true')
-            }
-        });
-
-        const userAnswer = element.getAttribute('value') - 1;
-        const abcd = ["a","b","c","d"];
-
-        if (element.matches("button") === true) {
-            if (abcd[userAnswer] === quizQuestions[questionNumber].correctAnswer){
-                score++;
-
-                rightWrong.textContent = "C O R R E C T" 
-                var correctClear = setInterval(function () {
-                    rightWrong.textContent = ""
-                    clearInterval(correctClear);
-                }, 2000);
-                
-                nextQuestion();
-            }
-            else{
-                timer = timer - 10;
-
-                rightWrong.textContent = "I N C O R R E C T"
-                var wrongClear = setInterval(function () {
-                    rightWrong.textContent = ""
-                    clearInterval(wrongClear);
-                }, 3000);
-                nextQuestion();
-            }
-        }
-    })
-
-}
-
 function nextQuestion () {
     var nextQuestion = setInterval(function(){
         //count up for the questions 
@@ -199,7 +122,7 @@ function nextQuestion () {
     
 }
 
-function submitScore() {
+function submitScore() { 
     //make sure correct and incorrect text is cleared 
     rightWrong.textContent = "";
     //swap pages 
@@ -212,7 +135,7 @@ function submitScore() {
     finalScore.textContent = score; 
 }
 
-function clearButtons () {
+function clearButtons () { //clear question buttons and recreate ol for new buttons 
     //clear old buttons
     var removeButton = document.getElementById("answers");
     removeButton.remove();
@@ -224,7 +147,8 @@ function clearButtons () {
     answerSelect = document.querySelector("#answers"); 
 }
 
-function clearScoreList () {
+function clearScoreList () { //clear score list
+    //removes the ol element containing the highscore values
     var removeHighscore = document.getElementById("scoreList");
     removeHighscore.remove();
 
@@ -235,7 +159,89 @@ function clearScoreList () {
     scoreList = document.querySelector("#scoreList");
 }
 
-startQuiz.addEventListener("click", function(){
+//show the quiz content on the page and check answers 
+function quizGenerate() {
+    //checks to see if we are at the end of questions, if we are end function
+    if (questionNumber > quizQuestions.length){ //
+        clearButtons ();
+        return;
+    }
+    //reset timer value so that it will countdown
+    timerStop = false;
+    
+    //set the text to the current question
+    questionSelect.textContent= quizQuestions[questionNumber].question;
+    
+    var buttonNumber = 0; 
+    for(answer in quizQuestions[questionNumber].answers) {
+        //generate buttonNumber for comparison later
+        buttonNumber++;
+        
+        //create list elements
+        var liElem = document.createElement("li");
+        answerSelect.appendChild(liElem);
+        answerSelect.setAttribute("style", "list-style: none;");
+        answerSelect.setAttribute("class", "answerList");
+        
+        //create buttons inside the list elements 
+        var button = document.createElement("button");
+        button.setAttribute("value", buttonNumber);
+        button.setAttribute("class", "answerButton");
+        button.setAttribute("style", "width: 80%; list-style:none; padding-left:0; text-align: left; position:relative; left:5%");
+
+        //set the content of the button and append it
+        button.innerHTML = `${answer}: ${quizQuestions[questionNumber].answers[answer]}`;     
+        liElem.appendChild(button);
+    }
+    buttonNumber = 0; //reset buttonNumber value for the next iteration
+
+    
+    //event listener to check if answer clicked is correct 
+    answerSelect.addEventListener("click", function(event) {
+        var element = event.target;  //create variable to see which element is being clicked
+        
+        //disables all the answer buttons after clicked once 
+        buttons = document.querySelectorAll('button');
+        buttons.forEach((answerButton) => {
+            if( answerButton.getAttribute("class") === "answerButton"){
+                answerButton.setAttribute('disabled', 'true')
+            }
+        });
+
+
+        const userAnswer = element.getAttribute('value') - 1;
+        const abcd = ["a","b","c","d"];
+
+        //if the element clicked is a button do this 
+        if (element.matches("button") === true) {
+            //if the answer is correct do this 
+            if (abcd[userAnswer] === quizQuestions[questionNumber].correctAnswer){
+                score++;
+
+                rightWrong.textContent = "C O R R E C T" 
+                var correctClear = setInterval(function () {
+                    rightWrong.textContent = ""
+                    clearInterval(correctClear);
+                }, 2000);
+                
+                nextQuestion();
+            }
+            else{ //if the answer is incorrect do this 
+                timer = timer - 10;
+
+                rightWrong.textContent = "I N C O R R E C T"
+                var wrongClear = setInterval(function () {
+                    rightWrong.textContent = ""
+                    clearInterval(wrongClear);
+                }, 3000);
+                nextQuestion();
+            }
+        }
+    })
+
+}
+
+startQuiz.addEventListener("click", function(){ //start the quiz when this button is clicked :)
     //reset values to default 
     clearScoreList();
     clearButtons ();
@@ -257,12 +263,13 @@ startQuiz.addEventListener("click", function(){
         timer--;
         timeStart.textContent = timer; 
 
+        //if timer equals 0 or less than 0 do this 
         if (timer <= 0) {            
             clearInterval(timerCountdown);
             submitScore ();
     
             quizPage.setAttribute("style", "display:none");
-            highscorePageSubmit.setAttribute("style", "display:visible"); //lose, still register score 
+            highscorePageSubmit.setAttribute("style", "display:visible"); 
         } else if (timerStop === true){
             clearInterval(timerCountdown)
         }    
@@ -271,7 +278,7 @@ startQuiz.addEventListener("click", function(){
     
 })
 
-submitScores.addEventListener("click", function(event) {
+submitScores.addEventListener("click", function(event) { //submit initial and score to be saved when this button clicked
     event.preventDefault();//prevent webpage from refreshing
     //check for empty field 
     if (initialInput.value === "") {
@@ -279,22 +286,24 @@ submitScores.addEventListener("click", function(event) {
         return;
       }
 
+    //push the input and the score into their respective arrays
     recordedScore[0].push(initialInput.value.trim()); 
     recordedScore[1].push(score);
 
+    //save the object to local storage as a string to recall for later 
+    localStorage.setItem("score", JSON.stringify(recordedScore)); 
     
-    localStorage.setItem("score", JSON.stringify(recordedScore)); //save the object to local storage to recall for later
-    
+    //create for loop to iterate through the arrays. since score and initials are always equal pick one to grab length for 
     for (var index = 0; index < recordedScore[0].length; index++) {
 
         //var initialListStore =  recordedScore[0][index]; //loop through initials 
         //var scoreListStore = recordedScore[1][index]; //loop through scores
 
         var showScores = document.createElement("li"); //create li
-        showScores.setAttribute("data-index", index);
-        showScores.textContent = `${recordedScore[0][index]} - ${recordedScore[1][index]}`;
+        //showScores.setAttribute("data-index", index); 
+        showScores.textContent = `${recordedScore[0][index]} - ${recordedScore[1][index]}`;//set text depending on index value
 
-        var newScore = JSON.parse(localStorage.getItem("score")); //parse stored object and save as an object var 
+        //var newScore = JSON.parse(localStorage.getItem("score"));
         
         scoreList.appendChild(showScores); //append into existence
     }
@@ -304,28 +313,26 @@ submitScores.addEventListener("click", function(event) {
     highscorePage.setAttribute("style", "display:visible");
     })
 
-clearScores.addEventListener("click", function(event) {
+clearScores.addEventListener("click", function(event) { //clear scores array
     clearScoreList ();
     recordedScore =[[],[]]
 })
 
-
-goBack.addEventListener("click", function(event) {
+goBack.addEventListener("click", function(event) { //go back to main page
     introductionPage.setAttribute("style", "display:visible")
     highscorePage.setAttribute("style", "display:none");
 })
 
-viewHighscore.addEventListener("click", function(event) {
-    event.preventDefault();
-    timerStop = true;
+viewHighscore.addEventListener("click", function(event) { //view the highscore page 
+    event.preventDefault(); //prevent page refresh 
+    timerStop = true; //stop timer 
 
-    if (highscorePageSubmit.getAttribute("display") === "visible" || highscorePage.getAttribute === "visible"){
+    //disable this on specific pages 
+    if (highscorePageSubmit.getAttribute("display") === "visible" || highscorePage.getAttribute === "visible"){ 
         return;
     }
 
     introductionPage.setAttribute("style", "display: none");
     quizPage.setAttribute("style", "display: none");
     highscorePage.setAttribute("style", "display:visible");
-
-
 })
